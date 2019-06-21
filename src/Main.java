183a6790fde19b38a1a899469664b55f591bec0c
Przemysław Nagórek <pnagorek@BOC.Group>
2019-06-21 09:47:47 +0200

    remove java files

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@ public class Main {
   @SuppressWarnings( "deprecation" )
   static public void main(String argv[]) {    
     try {
-      System.out.println("READY!");
+      System.out.println("==========================================");
+      System.out.println("==================READY!==================");
+      System.out.println("==========================================");
       Parser p = new Parser(new Lexer(new InputStreamReader(System.in)));
       Object result = p.parse().value;      
     } catch (Exception e) {
